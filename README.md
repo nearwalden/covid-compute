b291f228ab923d02c45bc22b2d81d64c3157d35d
David Douglas <nearwalden@gmail.com>
2020-03-16 22:28:42 -0400

    first pass, still documenting

--- README.md
+++ README.md
@@ -1,16 +1,33 @@
 # COVID Data Analysis
 
-This repo contains some routines for analyzing COVID-19 data.
+This repo contains some routines that assist in analyzing COVID-19 data.  It works with the Johns Hopkins data and organizes it into pandas dataframes that are useful to work with. 
 
 ## Prerequisites
 
 This code was written for Python 3.  The following packages are required:
 
-- Pandas
-- Matplotlib
+- pandas
+- matplotlib
 
 ## Data Sources
 
 [This page](DATA.md) describes the sources for the data used here.
 
 ## Usage
+
+To get started:
+
+1.  Install the prerequisites if needed
+2.  Clone the data repo somewhere on your system
+3.  Clone this repo somewhere else
+
+Note that once you've cloned the data repo, you can just do `git pull origin master` to get the new data each day.
+
+Next, create a `CovidData` object that cleans up all of the data and makes a bunch of pandas dataframes.  It takes one argument, which is the path to the data repo, including the trailing `/`:
+
+```python
+import covid
+cd = covid.CovidData('/home/dd/git/')  # assumes data is in /home/dd/git/COVID-19...
+```
+
+
--- covid.py
+++ covid.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import states
 
 # this should be set to the location where you have cloned the repo above
-COVID_19_REPO_PATH = "/Users/dd/gitcode/COVID-19/csse_covid_19_data/csse_covid_19_time_series/"
+COVID_19_REPO_PATH = "COVID-19/csse_covid_19_data/csse_covid_19_time_series/"
 
 # confirmed file
 COVID_19_CONFIRMED = "time_series_19-covid-Confirmed.csv"
@@ -14,12 +14,18 @@ COVID_19_CONFIRMED = "time_series_19-covid-Confirmed.csv"
 # confirmed file
 COVID_19_DEATHS = "time_series_19-covid-Deaths.csv"
 
-class covid_data:
-    def __init__(self):
-        self.confirmed = p.read_csv(COVID_19_REPO_PATH + COVID_19_CONFIRMED)
-        self.deaths = p.read_csv(COVID_19_REPO_PATH + COVID_19_DEATHS)
+class CovidData:
+    def __init__(self, base_path):
+        self.confirmed = p.read_csv(base_path + COVID_19_REPO_PATH + COVID_19_CONFIRMED)
+        self.deaths = p.read_csv(base_path + COVID_19_REPO_PATH + COVID_19_DEATHS)
+        self.confirmed_us = self.summarize_us (self.confirmed)
+        self.confirmed_countries = self.summarize_countries (self.confirmed)
+        self.deaths_us = self.summarize_us (self.deaths)        
+        self.deaths_countries = self.summarize_countries (self.deaths)
+        self.states = self.make_indiv_tables (self.confirmed_us, self.deaths_us)
+        self.countries = self.make_indiv_tables (self.confirmed_countries, self.deaths_countries)
 
-    def summarize_us (df_in):
+    def summarize_us (self, df_in):
         df_states = df_in [df_in['Country/Region'] == 'US'].copy()
         df_states['State'] = df_states['Province/State'].map(states.NAME_MAP)
         df_states = df_states.drop(['Country/Region', 'Lat', 'Long', 'Province/State'], axis=1)
@@ -28,19 +34,44 @@ class covid_data:
         df_us['US'] = df_us.sum(axis=1)
         return df_us
 
-    def summarize_countries (df_in):
+    def summarize_countries (self, df_in):
         df_simpler = df_in.drop(['Province/State', 'Lat', 'Long'], axis=1)
         df_simpler_g = df_simpler.groupby('Country/Region')
         df = df_simpler_g.sum().copy()
         return df.T
 
+    def make_indiv_tables (self, confirmed, deaths):
+        tables = {}
+        for name in confirmed.columns:
+            new_df = p.DataFrame()
+            new_df['confirmed'] = confirmed[name]
+            new_df['deaths'] = deaths[name]
+            tables[name] = new_df
+        return tables
 
-def plot (df, params, title):
-    fig, ax = plt.subplots()
-    for param in params:
-            ax.plot(df.index, df[param])
-    ax.set_title (title)
-    plt.show()
+    def plot_states (self, states, conf_or_death):
+        if conf_or_death == 'confirmed':
+            df = self.confirmed_us
+        else:
+            df = self.deaths_us
+        fig, ax = plt.subplots()
+        for state in states:
+            ax.plot(df.index, df[state], marker='o')
+        ax.legend()
+        ax.set_title ('State cases (' + conf_or_death + ')')
+        plt.show()
+
+    def plot_countries (self, countries, conf_or_death):
+        if conf_or_death == 'confirmed':
+            df = self.confirmed_countries
+        else:
+            df = self.deaths_countries
+        fig, ax = plt.subplots()
+        for country in countries:
+            ax.plot(df.index, df[country], marker='o')
+        ax.legend()
+        ax.set_title ('Country cases (' + conf_or_death + ')')
+        plt.show()
 
 
 
